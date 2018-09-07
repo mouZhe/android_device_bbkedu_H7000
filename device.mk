09b5d971bd22c802c3b0849b91744a2fffe4e13c
LuK1337 <priv.luk@gmail.com>
2018-09-07 15:21:20 +0200

    H7000: Drop libbt-vendor
    * With QTI bluetooth it's no longer needed.
    
    Change-Id: Iba4b0b6ba24fe1b85e7175291ed84769615ffadd

--- device.mk
+++ device.mk
@@ -132,10 +132,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
 PRODUCT_PACKAGES += \
     Snap
 
