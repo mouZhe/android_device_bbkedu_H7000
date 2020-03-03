725f919b54c72de2aa05ea22be1510c2a84fada3
John Luo <lonelyjskj@gmail.com>
2020-03-03 20:16:38 +0800

    H7000: rootdir: Support A/B fstab
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,7 +229,11 @@ BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+ifeq ($(AB_OTA_UPDATER), true)
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_AB.qcom
+else
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
+endif
 BOARD_HAS_LARGE_FILESYSTEM := true
 
 # RIL
